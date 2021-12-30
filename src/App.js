2e7712908c5fa94bce184f99b41f6974ddd90370
Guilherme Viana <guilhermehviana01@gmail.com>
2021-12-30 16:11:20 -0300

    Add app routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Cart from './Pages/Cart';
+import Details from './Pages/Details';
+import Home from './Pages/Home';
+import NotFound from './Pages/NotFound';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+      <div className='app'>
+        <Switch>
+          <Route exact path="/" component={ Home }/>
+          <Route exact path="/cart" component={ Cart }/>
+          <Route exact path="/details/:id" render={ (props) => <Details {...props} /> } />
+          <Route component={ NotFound }/>
+        </Switch>
+      </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
